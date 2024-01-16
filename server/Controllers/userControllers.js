067b3791207101cf211cdfd6b78a57c3e4258341
Jatin Foujdar <jatin321001@gmail.com>
2024-01-16 21:48:16 +0530

    hashed password commit

--- server/Controllers/userControllers.js
+++ server/Controllers/userControllers.js
@@ -1,4 +1,5 @@
 import User from "../Models/userModel.js"
+import bcrypt from "bcryptjs";
 
 
 const signupUser = async(req,res)=>{
@@ -9,8 +10,28 @@ const signupUser = async(req,res)=>{
         if(user){
             return res.status(400).json({message: "User already exists"});
         }
-        
-        
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new User({
+            name, email, username, password:hashedPassword,
+        });
+
+        await newUser.save();
+
+        if(newUser){
+            res.status(201).json({
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                username: newUser.username,
+            })
+        }
+        else{
+            res.status(400).json({message:" Invalid User data"});
+        }
+
      } catch (error) {
         res.status(500).json({message: error.message})
         console.log("Error in SignupUser:" , error.message);
