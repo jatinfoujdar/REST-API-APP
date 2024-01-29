5b8388b62d700e377f281d186d34b9bf0e95058f
Jatin Foujdar <jatin321001@gmail.com>
2024-01-29 22:38:14 +0530

    logout commit

--- server/Controllers/userControllers.js
+++ server/Controllers/userControllers.js
@@ -64,7 +64,15 @@ const loginUser = async(req,res)=>{
     }
 }
 
+const logoutUser = async(res,res)=>{
+  try {
+    res.cookie("jwt","",{maxAge:1});
+    res.status(200).json({message: "User logged out successfully"});
+  } catch (error) {
+    res.status(500).json({message: error.message})
+    console.log("Error in logoutUser:" , error.message);
+  }
+}
 
 
-
-export {signupUser , loginUser};
+export {signupUser , loginUser , logoutUser};
